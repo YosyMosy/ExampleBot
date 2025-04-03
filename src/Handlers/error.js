29667834319e69948deb760b9920ad4c08bea1ea
Mehmet <63320170+memte@users.noreply.github.com>
2025-04-03 17:02:01 +0300

    Create error.js

---
+++ src/Handlers/error.js
@@ -0,0 +1,17 @@
+module.exports = {
+  execute(client) {
+    const handleError = (type, error) => {
+      logger.error(`${type}: ${error.stack || error.message}`);
+    };
+
+    const processEvents = {
+      unhandledRejection: "Unhandled promise rejection",
+      uncaughtException: "Uncaught exception",
+      uncaughtExceptionMonitor: "Uncaught exception monitored"
+    };
+
+    for (const [event, message] of Object.entries(processEvents)) {
+      process.on(event, (error) => handleError(message, error));
+    }
+  },
+};
