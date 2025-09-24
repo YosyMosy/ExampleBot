f6bc59a7d9e00e3f1f5310bb3d5226b0b1375d68
Mehmet <63320170+memte@users.noreply.github.com>
2025-09-24 14:37:35 +0300

    Update client.js

--- src/Base/client.js
+++ src/Base/client.js
@@ -1,7 +1,32 @@
-const BaseClient = require("./client.js");
-const config = require("./config.js");
+const { readdirSync } = require("node:fs");
+const { Client, Intents } = require("discord.js");
 
-const token = config.token;
-const client = new BaseClient(token);
+module.exports = class BaseClient {
+  constructor(token) {
+    this.client = new Client({
+      intents: Object.values(Intents.FLAGS),
+      partials: [
+        "USER",
+        "CHANNEL",
+        "GUILD_MEMBER",
+        "MESSAGE",
+        "REACTION",
+        "GUILD_SCHEDULED_EVENT",
+      ],
+      shards: "auto",
+    });
+    this.token = token;
+  }
 
-client.start();
+  loadHandlers() {
+    readdirSync("./src/Handlers").forEach(async (file) => {
+      const handler = await require(`../Handlers/${file}`);
+      handler.execute(this.client);
+    });
+  }
+
+  start() {
+    this.loadHandlers();
+    this.client.login(this.token);
+  }
+}
