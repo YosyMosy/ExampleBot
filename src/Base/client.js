e424e38d397185fde2a8344417177dbf7e7ea391
Mehmet <63320170+memte@users.noreply.github.com>
2025-09-24 14:48:11 +0300

    biome support

--- src/Base/client.js
+++ src/Base/client.js
@@ -2,25 +2,25 @@ import { readdirSync } from "node:fs";
 import { Client, GatewayIntentBits, Partials } from "discord.js";
 
 export default class BaseClient {
-    constructor(token) {
-        this.client = new Client({
-            intents: Object.values(GatewayIntentBits),
-            partials: Object.values(Partials),
-            shards: "auto",
-        });
-        this.token = token;
-    }
+	constructor(token) {
+		this.client = new Client({
+			intents: Object.values(GatewayIntentBits),
+			partials: Object.values(Partials),
+			shards: "auto",
+		});
+		this.token = token;
+	}
 
-    loadHandlers() {
-        readdirSync("./src/Handlers").forEach(async (file) => {
-            const handlerFile = await import(`../Handlers/${file}`);
-            const handler = handlerFile.default;
-            handler.execute(this.client);
-        });
-    }
+	loadHandlers() {
+		readdirSync("./src/Handlers").forEach(async (file) => {
+			const handlerFile = await import(`../Handlers/${file}`);
+			const handler = handlerFile.default;
+			handler.execute(this.client);
+		});
+	}
 
-    start() {
-        this.loadHandlers();
-        this.client.login(this.token);
-    }
+	start() {
+		this.loadHandlers();
+		this.client.login(this.token);
+	}
 }
