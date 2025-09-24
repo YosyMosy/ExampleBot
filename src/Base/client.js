a0abc0acf5fd7da887ea89129af1598df754d005
Mehmet <63320170+memte@users.noreply.github.com>
2025-09-24 14:53:02 +0300

    biome support

--- src/Base/client.js
+++ src/Base/client.js
@@ -2,32 +2,32 @@ import { readdirSync } from "node:fs";
 import { Client, Intents } from "discord.js";
 
 export default class BaseClient {
-  constructor(token) {
-    this.client = new Client({
-      intents: Object.values(Intents.FLAGS),
-      partials: [
-        "USER",
-        "CHANNEL",
-        "GUILD_MEMBER",
-        "MESSAGE",
-        "REACTION",
-        "GUILD_SCHEDULED_EVENT",
-      ],
-      shards: "auto",
-    });
-    this.token = token;
-  }
+	constructor(token) {
+		this.client = new Client({
+			intents: Object.values(Intents.FLAGS),
+			partials: [
+				"USER",
+				"CHANNEL",
+				"GUILD_MEMBER",
+				"MESSAGE",
+				"REACTION",
+				"GUILD_SCHEDULED_EVENT",
+			],
+			shards: "auto",
+		});
+		this.token = token;
+	}
 
-  loadHandlers() {
-    readdirSync("./src/Handlers").forEach(async (file) => {
-      const handlerFile = await import(`../Handlers/${file}`);
-      const handler = handlerFile.default;
-      handler.execute(this.client);
-    });
-  }
+	loadHandlers() {
+		readdirSync("./src/Handlers").forEach(async (file) => {
+			const handlerFile = await import(`../Handlers/${file}`);
+			const handler = handlerFile.default;
+			handler.execute(this.client);
+		});
+	}
 
-  start() {
-    this.loadHandlers();
-    this.client.login(this.token);
-  }
+	start() {
+		this.loadHandlers();
+		this.client.login(this.token);
+	}
 }
