b37c7509b2bc35606a6d8815a8762369d8c26fc3
Mehmet <63320170+memte@users.noreply.github.com>
2025-11-19 01:54:26 +0300

    Update ping.js

--- src/Commands/info/ping.js
+++ src/Commands/info/ping.js
@@ -6,7 +6,7 @@ export const commandBase = {
 		aliases: ["pong"],
 	},
 	slashData: new SlashCommandBuilder().setName("ping").setDescription("Pong!"),
-	// If you want to improve the command, check the guide: https://v13.discordjs.guide/interactions/slash-commands.html#options
+	// If you want to improve the command, check the guide: https://discordjs.guide/legacy/slash-commands/advanced-creation (v14 guide)
 	cooldown: 5000, // 1 second = 1000 ms / set to 0 if you don't want a cooldown.
 	ownerOnly: false, // Set to true if you want the command to be usable only by the developer.
 	async prefixRun(message, args) {
